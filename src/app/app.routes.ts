96001959d3049eda8d8cb9ae3cedc533bbb4b748
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-08-11 11:18:32 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
-
 import { Routes } from '@angular/router';
 import { TraderPlanGeneratorComponent } from './trader-plan-generator/trader-plan-generator.component';
 
 export const routes: Routes = [
-	{ path: 'plan', component: TraderPlanGeneratorComponent },
-	{ path: '', redirectTo: 'plan', pathMatch: 'full' }
+  { path: '', component: TraderPlanGeneratorComponent },
 ];
